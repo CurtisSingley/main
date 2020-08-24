393110c0cbe4e51de9303c2dd509a440514d2168
Curtis Singley <CurtisSingley@gmail.com>
2020-08-24 10:04:09 -0700

    modified systembackup script to delete source files that were removed, and included more excluded directories

--- usr/local/sbin/systembackup.sh
+++ usr/local/sbin/systembackup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/mntHDD/*","/mntWIN/*","/media/*","/lost+found","/home/*/.cache/chromium/*"} / /media/Backups && \
-	tar -cvpzf /mntHDD/archsystembackup.tar.gz /media/Backups
+rsync -aAXHv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/var/run/*","/var/lock/*","/mnt/*","/mntHDD/*","/mntWIN/*","/media/*","/lost+found","/home/*/.cache/chromium/*","coredump/"} / /media/Backups --delete && \
+	tar -cvpzf /mntHDD/archsystembackup.tar.gz /media/Backups 
