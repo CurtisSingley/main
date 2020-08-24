282bac4845aba01bdd50ef792a2426ebf2ee1aad
Curtis Singley <CurtisSingley@gmail.com>
2020-08-24 10:24:38 -0700

    added var/tmp to excluded backup folders

--- usr/local/sbin/systembackup.sh
+++ usr/local/sbin/systembackup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -aAXHv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/var/run/*","/var/lock/*","/mnt/*","/mntHDD/*","/mntWIN/*","/media/*","/lost+found","/home/*/.cache/chromium/*","coredump/"} / /media/Backups --delete && \
+rsync -aAXHv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/var/run/*","/var/lock/*","/var/tmp/*","/mnt/*","/mntHDD/*","/mntWIN/*","/media/*","/lost+found","/home/*/.cache/chromium/*","coredump/"} / /media/Backups --delete && \
 	tar -cvpzf /mntHDD/archsystembackup.tar.gz /media/Backups 
